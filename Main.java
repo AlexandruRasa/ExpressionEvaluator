89dc31cabd5d2701ca5b837f4e2352bb6b1d027e
AlexandruRașa <114824311+AlexandruRasa@users.noreply.github.com>
2022-12-12 02:21:26 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,21 +7,5 @@ public class Main {
         test0.calculateResult();
         test0.setInput("3 + (2 - 5) - 1");
         System.out.println(test0.calculateResult());
-
-        int[] arr = {1000,4,25,319,88,51,3430,8471,701,1,2989,657,713};
-        int[] arr1 = {1,22,8,6,44,9};
-        RadixSort x = new RadixSort(arr);
-        x.sort();
-        x.printArr();
-        x.setNumbers(arr1);
-        System.out.println();
-        x.sort();
-        x.printArr();
-
-
-
     }
-
-
-
 }
